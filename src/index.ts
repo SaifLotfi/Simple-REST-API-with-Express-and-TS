a9a93f4bf0c4779e97c73d7b81fea36b4c3fac95
Saif <swaiflnkac@gmail.com>
2023-12-07 11:48:44 +0200

    basic setup

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+
+const app = express();
+
+app.use(cors({ credentials: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(compression());
+const server = app.listen(process.env.PORT || 5000, () => {
+    console.log(`Server is running on port ${process.env.PORT || 5000}`);
+});
