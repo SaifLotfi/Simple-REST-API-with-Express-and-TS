3e20cb3c216ed6214db9e80b0c82dde9cd6ae265
Saif <swaiflnkac@gmail.com>
2023-12-12 19:52:51 +0200

    added global error handler

---
+++ src/errors/index.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+const globalErrorHandler = (
+    err: any,
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Something went wrong';
+    res.status(statusCode).json({ message });
+};
