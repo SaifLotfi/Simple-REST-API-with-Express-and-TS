b5c0073e152970a76aa0587f212ec0571f15cf74
Saif <swaiflnkac@gmail.com>
2023-12-12 21:27:24 +0200

    added login functionality

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import 'express-async-errors';
-import { registerUser } from '../services/authentication.service';
+import { registerUser, loginUser } from '../services/authentication.service';
 const register = async (req: express.Request, res: express.Response) => {
     const { name, email, password } = req.body;
 
@@ -9,4 +9,17 @@ const register = async (req: express.Request, res: express.Response) => {
     res.status(200).json(user).end();
 };
 
-export { register };
+const login = async (req: express.Request, res: express.Response) => {
+    const { email, password } = req.body;
+
+    const user = await loginUser(email, password);
+
+    res.cookie('sessionToken', user.authentication.sessionToken, {
+        domain: process.env.HOST,
+        path: '/',
+    });
+
+    res.status(200).json(user).end();
+};
+
+export { register ,login};
--- src/services/authentication.service.ts
+++ src/services/authentication.service.ts
@@ -2,10 +2,10 @@ import { createUser, getUserByEmail } from '../models/user';
 import { authentication, random } from '../helpers';
 import 'express-async-errors';
 const registerUser = async (name: string, email: string, password: string) => {
-
     if (!name || !email || !password) {
         const err: any = new Error('name, email and password are required');
         err.status = 400;
+        throw err;
     }
 
     //check if user already exists
@@ -13,6 +13,7 @@ const registerUser = async (name: string, email: string, password: string) => {
     if (existingUser) {
         const err: any = new Error('user already exits');
         err.status = 400;
+        throw err;
     }
 
     // create user
@@ -29,6 +30,35 @@ const registerUser = async (name: string, email: string, password: string) => {
     return user;
 };
 
-export {
-    registerUser,
-}
+const loginUser = async (email: string, password: string) => {
+    //check if any parameter is missing
+    if (!email || !password) {
+        const err: any = new Error('email and password are required');
+        err.status = 400;
+        throw err;
+    }
+
+    const user = await getUserByEmail(email).select('+authentication.salt +authentication.password');
+
+    if (!user) {
+        const err: any = new Error('user not found');
+        err.status = 404;
+        throw err;
+    }
+
+    const expectedPassword = authentication(user.authentication.salt, password);
+
+    if(expectedPassword !== user.authentication.password){
+        const err: any = new Error('invalid password');
+        err.status = 403;
+        throw err;
+    }
+
+    const salt = random();
+    user.authentication.sessionToken = authentication(salt, user._id.toString());
+    
+    await user.save();
+
+    return user;
+};
+export { registerUser, loginUser };
